10a38d2daddfb12d9d417a9d309b95f0ae11479e
Nworm <1574242600@qq.com>
2020-10-31 21:14:34 +0800

    feat: 贴吧签到

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const Fs = require('fs');
 const CryptoJS = require("crypto-js");
 
 global.AESDecryp = (ciphertext) => {
-    const decrypKey = process.argv[2];
-    let bytes  = CryptoJS.AES.decrypt(ciphertext, decrypKey);
+    const decryp_key = process.argv[2];
+    let bytes = CryptoJS.AES.decrypt(ciphertext, decryp_key);
     return bytes.toString(CryptoJS.enc.Utf8);
 }
 
@@ -12,9 +12,12 @@ function getAllModules() {
 }
 
 (async () => {
-    for (let module of getAllModules()){
-        let msg = await require(`./modules/${module}/main`)();
-        console.log(`模块[${module}]:\n ${msg}`)
+    for (let module_name of getAllModules()) {
+        console.log(`模块[${module_name}]开始运行:`);
+        let msg = await require(`./modules/${module_name}/main`)();
+        console.log(`模块[${module_name}]:\n ${msg}`);
     }
-})()
+})().catch(e => {
+    console.error(e)
+})
 
---
+++ modules/tieba/main.js
@@ -0,0 +1,99 @@
+const fetch = require('node-fetch');
+const CryptoJS = require("crypto-js");
+
+let tbTotal = 0;
+let errConut = 0;
+const BDUSS = AESDecryp('U2FsdGVkX18RzV4LogquP5BIfvAKvM9nroQiV6nPXpfdetporGfzj9NDnqoIyUhNOZj5hnDZGqUK1PJUXIpijC5m0XHfLNLZJiKLuV27AyIKpF2Uyva/QGVY755pSvn+GSyV1R88IxNqoDGeOgJxth1V0rZBtbjwTYHRM+o5rEo4g2/LcNbJcCux5aZlNIkffd/4ZE09vwDudKbAt5sESacb6LGL9g4QYhMqQ5jqjVcZc6P4qKtHTHYWh1oU82PI43Scx/YNQjN3TwOphLZJAYf7HDTwIQJgfDBs4JGuszQ=');
+
+function toQueryString(data) {
+    return Object.keys(data)
+        .map((v) => `${v}=${encodeURIComponent(data[v])}`).join('&')
+}
+
+function getDataSignHash(data) {
+    const dataString = Object.keys(data)
+        .sort()
+        .map((v) => `${v}=${data[v]}`).join('');
+    return CryptoJS.MD5(dataString + 'tiebaclient!!!').toString().toUpperCase();
+}
+
+async function getTbs() {
+    return await fetch('https://tieba.baidu.com/dc/common/tbs', {
+        headers: {
+            Cookie: 'BDUSS=' + BDUSS
+        }
+    }).then(async v => (await v.json())['tbs']).catch((e) => { throw `tbs获取失败: \n ${e}` });
+}
+
+async function getTbList() {
+    let list = [];
+    let data = {
+        BDUSS: BDUSS,
+        _client_type: '2',
+        _client_id: 'wappc_1534235498291_488',
+        _client_version: '9.7.8.0',
+        _phone_imei: '000000000000000',
+        from: '1008621y',
+        page_no: 1,
+        page_size: '200',
+        model: 'MI+5',
+        net_type: '1',
+        timestamp: Math.round(new Date() / 1000),
+        vcode_tag: '11',
+    };
+
+    data.sign = getDataSignHash(data);
+    // todo 分页 
+    /* while(true) { const json = */await fetch('https://tieba.baidu.com/c/f/forum/like', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: toQueryString(data),
+    }).then(async v => {
+        const json = await v.json();
+        json['forum_list']['non-gconforum'].map(v => {
+            list.push(v)
+        });
+        //return json;
+    }).catch((e) => { throw `TbList获取失败: \n ${e}` })
+
+    return list;
+}
+
+async function signTb(fid, tbs, kw) {
+    const data = {
+        BDUSS: BDUSS,
+        fid: fid,
+        tbs: tbs,
+        kw: kw,
+        timestamp: Math.round(new Date() / 1000),
+    };
+    data.sign = getDataSignHash(data);
+
+    return await fetch('https://tieba.baidu.com/c/c/forum/sign', {
+        method: 'POST',
+        body: toQueryString(data)
+    }).then(async v => {
+        const json = await v.json();
+        if (json.error_code != 0) throw '签到失败';
+    }).catch(() => {
+        errConut++;
+    })
+}
+
+module.exports = async () => {
+    try {
+        const tbs = await getTbs();
+        const tbList = await getTbList();
+        tbTotal = tbList.length;
+        for (v of tbList) { 
+            await signTb(v.id, tbs, v.name);
+        }
+
+        return `共签到${tbTotal}个吧, 失败${errConut}个`;
+    } catch (e) {
+        return e;
+    }
+}
+module.exports()
